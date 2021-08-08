276c0702e57113ab9a51eb8ec9a312348408bfbe
Jiyeol Jake Lee <jake.lee4006@gmail.com>
2021-08-08 17:24:23 +0900

    BlockContent serializers에 link 핸들링 추가

--- components/BlogContent.js
+++ components/BlogContent.js
@@ -29,6 +29,18 @@ const serializers = {
       // }
       return null;
     },
+    link: ({ mark, children }) => {
+      // Read https://css-tricks.com/use-target_blank/
+      const { blank, href } = mark;
+
+      return blank ? (
+        <a href={href} target="_blank" rel="noopener">
+          {children}
+        </a>
+      ) : (
+        <a href={href}>{children}</a>
+      );
+    },
   },
 };
 
