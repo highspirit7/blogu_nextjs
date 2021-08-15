43c43632aad53a4c9bb3dbbfd4f6b69789a7d267
Jiyeol Jake Lee <jake.lee4006@gmail.com>
2021-08-15 12:54:11 +0900

    블로그 콘텐츠 내 이미지 max-width 100% 적용되도록 수정

--- components/BlogContent.js
+++ components/BlogContent.js
@@ -9,7 +9,7 @@ const serializers = {
     },
     image: ({ node: { asset, alt, position = "center" } }) => {
       return (
-        <div className={`blog-image-${position}`}>
+        <div className={`blog-image blog-image-${position}`}>
           <img src={urlFor(asset).height(300).fit("max").url()} />
           <div className="image-alt">{alt}</div>
         </div>
